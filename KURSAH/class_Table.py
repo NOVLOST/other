485f04ca174955c5f9024d161bf27c3c6cd3a151
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2024-10-21 20:35:53 +0400

    Update class_Table.py

--- KURSAH/class_Table.py
+++ KURSAH/class_Table.py
@@ -1,61 +1,112 @@
+
 import csv
+import math
 from class_Window import *
 
 coordinate_list = []
 table_list = []
-tree_list = []
+index_of_targets = []
+coordinate_multiplier= 0.1
+headrs = ('Время','Xo', 'Yo', 'Пелинг','Дистанция','X','Y')
 class Table():
 
-    def __init__(self):
-        self.headrs = ('Курс', 'Дистанция', 'Скорость', 'Время')
-        self.info = []
-
+    def __init__(self,i):
+        self.target_index = i
 
     def table_init(self):
+        self.tree = ttk.Treeview(height=3, show="headings", columns=headrs)
 
+        for head in headrs:
+            self.tree.heading(head, text=head, anchor='center')
 
-
-        coordinate_multiplier= 0.1
-
-        for data in table_list:
-            tree = ttk.Treeview(height=3, show="headings", columns=self.headrs)  # таблица c данными целей
-
-            tree.column("#1", stretch=NO, width=70)
-            tree.column("#2", stretch=NO, width=80)
-            tree.column("#3", stretch=NO, width=70)
-            tree.column("#4", stretch=NO, width=70)
-
-            tree.insert("", END, values=(data.info[0][0],data.info[0][1]))
-
-            tree_list.append(tree)
-
-            tree_list[-1].place(x=win.width // 2 + 20, y=win.height * coordinate_multiplier)
-
-
-            coordinate_multiplier += 0.1
+        for i in range(1, len(headrs) + 1):
+            if i == 5:
+                self.tree.column(f"#{i}", stretch=NO, width=80)
+            else:
+                self.tree.column(f"#{i}", stretch=NO, width=70)
 
 
 
 
 
 
-        for tab in tree_list:
-            for head in self.headrs:
-                tab.heading(head, text=head, anchor='center')
 
-    def read_cvs(self,file_name):
-        global coordinate_list
+
+def read_cvs(file_name):
+        global coordinate_list,coordinate_multiplier
+
         with open(file_name, 'r') as file:
-            coordinate_list = csv.reader(file)
-            coordinate_list = list(coordinate_list)
 
-        print(coordinate_list)
+            while True:
 
-        for i in range(0,len(coordinate_list[0]),2):
-            table = Table()
-            table.info.append((coordinate_list[0][i],coordinate_list[0][i+1]))
+                coordinate_list = file.readline()
+                coordinate_list = coordinate_list.split(',')
+                print(coordinate_list)
 
-            table_list.append(table)
-        print(len(table_list))
 
-target_table = Table()
+                if len(coordinate_list)==1:
+                    break
+
+
+                else:
+
+                    if coordinate_list[-1] not in index_of_targets:
+
+                        index_of_targets.append(coordinate_list[-1])
+
+                        table = Table(coordinate_list[-1])
+                        table.table_init()
+                        table.tree.insert("", END, values=(coordinate_list[0], coordinate_list[1],coordinate_list[2],coordinate_list[3]))
+
+                        table_list.append(table)
+
+                        table_list[-1].tree.place(x=win.width // 2 + 20, y=win.height * coordinate_multiplier)
+
+                        coordinate_multiplier += 0.1
+
+
+                    else:
+
+                        for tab in table_list:
+                            if tab.target_index == coordinate_list[-1]:
+                                tab.tree.insert("", END, values=(coordinate_list[0], coordinate_list[1],coordinate_list[2],coordinate_list[3]))
+
+
+                        calculation()
+
+
+def right_triangle(xo,yo,xm,ym):
+
+        leg_a = max(xo,xm) - min(xo,xm)
+        leg_b = max(yo, ym) - min(yo, ym)
+        print(leg_a,leg_b)
+
+        hypotenuse = ((leg_a**2) + (leg_b**2))**0.5
+        print(f'hyp = {hypotenuse}')
+
+
+
+
+
+def calculation():
+
+        # xo,yo наша координата xm,ym координата маяка
+        # sin_po пелинг отновительно нашего коробля sin_pm пелинг относительно маяка
+        # side_a сторона треугольника от нас до цели ,side_b от нас до маяка,side_c от маяка до цели
+
+        xo = int(coordinate_list[1])
+        yo = int(coordinate_list[2])
+        sin_po = math.sin(int(coordinate_list[3]))
+
+        xm = int(coordinate_list[4])
+        ym = int(coordinate_list[5])
+        sin_pm = int(coordinate_list[6])
+
+
+        right_triangle(xo,yo,xm,ym)
+
+
+
+
+
+
