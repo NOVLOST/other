6a58c5c46659dca941be224b33bc98afddb97e80
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2024-10-22 18:09:56 +0400

    Update class_Table.py

--- KURSAH/class_Table.py
+++ KURSAH/class_Table.py
@@ -1,5 +1,5 @@
 
-import csv
+
 import math
 from class_Window import *
 
@@ -26,12 +26,6 @@ class Table():
                 self.tree.column(f"#{i}", stretch=NO, width=70)
 
 
-
-
-
-
-
-
 def read_cvs(file_name):
         global coordinate_list,coordinate_multiplier
 
@@ -56,7 +50,11 @@ def read_cvs(file_name):
 
                         table = Table(coordinate_list[-1])
                         table.table_init()
-                        table.tree.insert("", END, values=(coordinate_list[0], coordinate_list[1],coordinate_list[2],coordinate_list[3]))
+
+                        data_tuple = calculation()
+                        table.tree.insert("", END, values=(coordinate_list[0], coordinate_list[1],
+                                                           coordinate_list[2], coordinate_list[3],
+                                                        data_tuple[0],data_tuple[1], data_tuple[2]))
 
                         table_list.append(table)
 
@@ -64,49 +62,74 @@ def read_cvs(file_name):
 
                         coordinate_multiplier += 0.1
 
-
                     else:
 
                         for tab in table_list:
                             if tab.target_index == coordinate_list[-1]:
-                                tab.tree.insert("", END, values=(coordinate_list[0], coordinate_list[1],coordinate_list[2],coordinate_list[3]))
-
-
-                        calculation()
-
-
-def right_triangle(xo,yo,xm,ym):
-
-        leg_a = max(xo,xm) - min(xo,xm)
-        leg_b = max(yo, ym) - min(yo, ym)
-        print(leg_a,leg_b)
-
-        hypotenuse = ((leg_a**2) + (leg_b**2))**0.5
-        print(f'hyp = {hypotenuse}')
+                                data_tuple = calculation()
+                                tab.tree.insert("", END, values=(coordinate_list[0], coordinate_list[1]
+                                                                 ,coordinate_list[2],coordinate_list[3],
+                                                                 data_tuple[0],data_tuple[1],data_tuple[2]))
+                                print(calculation())
 
 
 
 
 
 def calculation():
-
-        # xo,yo наша координата xm,ym координата маяка
-        # sin_po пелинг отновительно нашего коробля sin_pm пелинг относительно маяка
-        # side_a сторона треугольника от нас до цели ,side_b от нас до маяка,side_c от маяка до цели
-
         xo = int(coordinate_list[1])
         yo = int(coordinate_list[2])
-        sin_po = math.sin(int(coordinate_list[3]))
 
         xm = int(coordinate_list[4])
         ym = int(coordinate_list[5])
-        sin_pm = int(coordinate_list[6])
 
+        leg_a = max(xo,xm) - min(xo,xm)
+        leg_b = max(yo, ym) - min(yo, ym)
+        print("-------------------\n\n\n",leg_a,leg_b)
 
-        right_triangle(xo,yo,xm,ym)
+        hypotenuse = ((leg_a**2) + (leg_b**2))**0.5
+        print(f'hyp = {hypotenuse}')
 
+        sin_b = (leg_a / hypotenuse)
 
+        rigTri_angle_mayak_in_degrees = math.degrees(math.asin(sin_b))
 
+        print("right angle = ",rigTri_angle_mayak_in_degrees)
 
+        mayak_peling = int(coordinate_list[6])
+        print("mayak peling  = ",mayak_peling)
+
+        interior_angel_mayak = 180 -  (mayak_peling + rigTri_angle_mayak_in_degrees)
+
+        print("need angel = ",interior_angel_mayak)
+
+        sin_a = (leg_b / hypotenuse)
+
+        print(f"------------------\n\nsin_a = {sin_a}")
+
+        rigTri_angle_me_in_degrees = math.degrees(math.asin(sin_a))
+
+        print(f"rightangle_me = {rigTri_angle_me_in_degrees}")
+
+        me_peling = int(coordinate_list[3])
+
+        print(f'me peling  = {me_peling}')
+
+        interior_angel_me = (90 -  rigTri_angle_me_in_degrees) + me_peling
+
+        print(f'need me angle = {interior_angel_me}')
+
+        print(f'sin me = {math.sin(math.radians(interior_angel_me))}')
+
+        distance_to_target = (hypotenuse * math.sin(math.radians(interior_angel_me))) / math.sin(math.radians(interior_angel_mayak))
+
+        print("distance = ",distance_to_target)
+
+        y_target_coordinate = (distance_to_target * math.sin(math.radians(90 - me_peling)))
+        x_target_coordinate = (distance_to_target * math.cos(math.radians(90 - me_peling))) + xo
+
+        print("coordinate finally = ",x_target_coordinate,y_target_coordinate)
+
+        return (round(distance_to_target,2),round(x_target_coordinate,2),round(y_target_coordinate,2))
 
 
